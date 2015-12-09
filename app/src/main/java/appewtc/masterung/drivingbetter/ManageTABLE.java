711c64e594b2c2e04f33abf98eed2cc557a56c0d
masterUNG <phrombutr@gmail.com>
2015-12-09 13:31:24 +0700

    Static Public Success

--- app/src/main/java/appewtc/masterung/drivingbetter/ManageTABLE.java
+++ app/src/main/java/appewtc/masterung/drivingbetter/ManageTABLE.java
@@ -12,6 +12,40 @@ public class ManageTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String TABLE_CAR = "carTABLE";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_Id_Car = "Id_Car";
+    private static final String COLUMN_Password = "Password";
+    private static final String COLUMN_MileCar = "MileCar";
+    private static final String COLUMN_Date = "Date";
+    private static final String COLUMN_Mile = "Mile";
+    private static final String COLUMN_ACT = "ACT";
+    private static final String COLUMN_TAX = "TAX";
+    private static final String COLUMN_Insure = "Insure";
+    private static final String COLUMN_Batt = "Batt";
+    private static final String COLUMN_Tire = "Tire";
+    private static final String COLUMN_Engine_oil = "Engine_oil";
+    private static final String COLUMN_Radiator = "Radiator";
+    private static final String COLUMN_Fullservice = "Fullservice";
+
+    public static final String TABLE_EMER = "emerTABLE";
+    private static final String COLUMN_ImgService = "ImgService";
+    private static final String COLUMN_TelService = "TelService";
+    private static final String COLUMN_ImgInsure = "ImgInsure";
+    private static final String COLUMN_TelInsure = "TelInsure";
+
+    public static final String TABLE_FIX = "fixTABLE";
+    private static final String COLUMN_Topig = "Topig";
+    private static final String COLUMN_ImageFix = "ImageFix";
+    private static final String COLUMN_DescripFix = "DescripFix";
+
+    public static final String TABLE_LOGIN = "loginTABLE";
+    private static final String COLUMN_ID_car_login = "ID_car_login";
+    private static final String COLUMN_TimeDate = "TimeDate";
+    private static final String COLUMN_Lat = "Lat";
+    private static final String COLUMN_Lng = "Lng";
+
+
     public ManageTABLE(Context context) {
 
         //Connected Database
