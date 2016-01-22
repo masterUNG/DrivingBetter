f9a68e9ffe8f238f1e8fdeee8a1668aa486bd0fe
masterUNG <phrombutr@gmail.com>
2016-01-22 09:46:50 +0700

    Design UI ListView Finish

--- app/src/main/java/appewtc/masterung/drivingbetter/RepairListView.java
+++ app/src/main/java/appewtc/masterung/drivingbetter/RepairListView.java
@@ -2,12 +2,28 @@ package appewtc.masterung.drivingbetter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RepairListView extends AppCompatActivity {
+
+    //Explicit
+    private ListView repairListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repair_list_view);
-    }
-}
+
+        //Bind Widget
+        repairListView = (ListView) findViewById(R.id.listView);
+
+        //Create ListView
+        createListView();
+
+    }   // Main Method
+
+    private void createListView() {
+
+    }   // createListView
+
+}   // Main Class
