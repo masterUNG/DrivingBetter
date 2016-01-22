8f24a95bc7866b1fa0ffc0908bc5a04cc465a557
masterUNG <phrombutr@gmail.com>
2016-01-22 14:56:40 +0700

    Finish 22Jan16

--- app/src/main/java/appewtc/masterung/drivingbetter/RepairListView.java
+++ app/src/main/java/appewtc/masterung/drivingbetter/RepairListView.java
@@ -14,6 +14,7 @@ public class RepairListView extends AppCompatActivity {
     //Explicit
     private ListView repairListView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
