ae00b227d39d8e6e28b17cf4ac24aaf0aa3112a5
masterUNG <phrombutr@gmail.com>
2016-01-22 14:44:15 +0700

    Start Map

--- app/src/main/java/appewtc/masterung/drivingbetter/ServiceCenterActivity.java
+++ app/src/main/java/appewtc/masterung/drivingbetter/ServiceCenterActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -29,6 +30,10 @@ public class ServiceCenterActivity extends AppCompatActivity {
         createListView();
 
     }   // Main Method
+
+    public void clickBackServiceCenter(View view) {
+        finish();
+    }
 
     private void createListView() {
 
