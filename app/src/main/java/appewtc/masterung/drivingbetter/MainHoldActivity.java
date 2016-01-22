8791ded11107f45a91934c33078d3174d7b23ebf
masterUNG <phrombutr@gmail.com>
2016-01-22 13:14:17 +0700

    Create Alert dialog finish

--- app/src/main/java/appewtc/masterung/drivingbetter/MainHoldActivity.java
+++ app/src/main/java/appewtc/masterung/drivingbetter/MainHoldActivity.java
@@ -1,5 +1,7 @@
 package appewtc.masterung.drivingbetter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,6 +45,43 @@ public class MainHoldActivity extends AppCompatActivity {
 
     public void clickCenterService(View view) {
 
-    }
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(this);
+        objBuilder.setIcon(R.drawable.icon_question);
+        objBuilder.setTitle(getResources().getString(R.string.tel));
+        objBuilder.setMessage("คุณต้องการไปที่ไหน ?");
+        objBuilder.setCancelable(false);
+        objBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.setNeutralButton("ศูนย์บริการ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent objIntent = new Intent(MainHoldActivity.this, ServiceCenterActivity.class);
+                objIntent.putExtra("Index", 1);
+                startActivity(objIntent);
+                dialogInterface.dismiss();
+
+            }   // event
+        });
+        objBuilder.setPositiveButton("ประกันภัย", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent obj2Intent = new Intent(MainHoldActivity.this, ServiceCenterActivity.class);
+                obj2Intent.putExtra("Index", 2);
+                startActivity(obj2Intent);
+                dialogInterface.dismiss();
+
+            }   // event
+        });
+
+        objBuilder.show();
+
+
+    }   // clickCenterService
 
 }   // Main Class
